8ef60e0d8a2e348c51e73efb3171d2756290b85b
earth <divine.icbm@gmail.com>
2020-04-03 22:11:11 -0500

    Update baba-is-javascript.js

--- baba-is-javascript.js
+++ baba-is-javascript.js
@@ -6,14 +6,20 @@ function reset() {
   
   game = {
     cash: new Decimal(0),
+    upgradesPurchased: []
   }
 }
 
 reset()
 
+function showUpgrade(id) {
+  document.getElementById(id).style.display = 'block';
+}
+
 function update() {
   document.getElementById("cash_number").innerHTML = game.cash
-  
+}
+
   if (screen.width < 900 || window.innerWidth < 900) {
     document.getElementById('cash').style.position = "absolute"
     document.getElementById('cash').style.top = "400px"
@@ -28,13 +34,20 @@ function update() {
 
 update()
 
-
+if (game.cash >= 10) {
+showUpgrade("upgrade1")
+} else {
+ document.getElementById("upgrade1").style.display = 'none'; 
+}
 
 
 
 
 
 function more_cash() {
+if (game.upgradesPurchased.includes("upgrade1")) {
+game.cash = game.cash.add(2)
+} else {
   game.cash = game.cash.add(1)
 }
 
