5ea3e045b7fd975cb6ce334a1202f7a64fd5ed98
earth <divine.icbm@gmail.com>
2020-04-04 09:30:28 -0500

    Update baba-is-javascript.js

--- baba-is-javascript.js
+++ baba-is-javascript.js
@@ -6,7 +6,8 @@ function reset() {
   
   game = {
     cash: new Decimal(0),
-    upgradesPurchased: []
+    upgradeCost: [10, 100, 500, 1000],
+    upgradesPurchased: [],
   }
 }
 
@@ -15,6 +16,8 @@ reset()
 function showUpgrade(id) {
   document.getElementById(id).style.display = 'block';
 }
+//defining upgrade showing as none
+document.getElementById("upgrade1").style.display = 'none';
 
 function update() {
   document.getElementById("cash_number").innerHTML = game.cash
@@ -34,17 +37,15 @@ function update() {
 update()
 
 if (game.cash >= 10) {
-showUpgrade("upgrade1")
-} else {
- document.getElementById("upgrade1").style.display = 'none'; 
-}
-
-if (game.upgradesPurchased.includes("upgrade1")) {
- game.cash -= 10 
+showUpgrade("upgrade1");
 }
 
 
-
+function purchaseUpgrade(id) {
+ if (game.upgradesPurchased.includes("upgrade1")) {
+  game.cash - game.upgradeCost[0]
+}
+}
 
 
 function moreCash() {
